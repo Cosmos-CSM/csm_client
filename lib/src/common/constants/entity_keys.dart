41e032de448b322cd0a0c1ee13db32aeba5dff91
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-04-17 16:57:07 -0700

    better encoding/decoding implementations

---
+++ lib/src/common/constants/entity_keys.dart
@@ -0,0 +1,11 @@
+/// [Class] constanst for [Entity] base shared properties keys access.
+final class EntityKeys {
+  /// Property binding for [Id].
+  static const String id = 'id';
+
+  /// Property binding for [Timestamp].
+  static const String timestamp = 'timestamp';
+
+  /// Property binding for [Discriminator].
+  static const String discriminator = 'discriminator';
+}
--- lib/src/service/entity_b.dart
+++ lib/src/service/entity_b.dart
@@ -1,4 +1,6 @@
 import 'package:csm_client/csm_client.dart';
+import 'package:csm_client/src/common/constants/entity_keys.dart';
+import 'package:meta/meta.dart';
 
 /// [Abstract] class for [Entities] base members handling and operations.
 abstract class EntityB implements EntityI {
@@ -13,4 +15,30 @@ abstract class EntityB implements EntityI {
   /// Entity handling timestamp.
   @override
   DateTime timestamp = DateTime.now();
+
+  @override
+  @mustCallSuper
+  @mustBeOverridden
+  JObject encode([JObject? entityObject]) {
+    JObject encode = <String, Object>{
+      EntityKeys.id: id,
+      EntityKeys.timestamp: timestamp,
+      EntityKeys.discriminator: discriminator,
+    };
+
+    if (entityObject != null) {
+      encode.addEntries(entityObject.entries);
+    }
+
+    return encode;
+  }
+
+  @override
+  @mustCallSuper
+  @mustBeOverridden
+  void decode(JObject encode) {
+    id = encode.get(EntityKeys.id);
+    timestamp = encode.get(EntityKeys.timestamp);
+    discriminator = encode.get(EntityKeys.discriminator);
+  }
 }
