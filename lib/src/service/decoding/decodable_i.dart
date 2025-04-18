c077c7ad29bd9492bf3affbb7b45534330fa1c1e
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-04-17 17:44:03 -0700

    removed unecessary properties

--- lib/src/service/decoding/decodable_i.dart
+++ lib/src/service/decoding/decodable_i.dart
@@ -3,9 +3,6 @@ import 'package:csm_client/csm_client.dart';
 /// [interface] for [Decodable] objects implementations, this interface provides
 /// members to generate an [TObject] based on an [Encoded] content.
 abstract interface class DecodableI {
-  /// Unique specific object identification.
-  late String discriminator;
-
   /// Loads the current object content from the given [encode] object.
   void decode(JObject encode);
 }
