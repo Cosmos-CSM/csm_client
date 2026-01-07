d92f957c6aaa2250f1526486727a81421cb7a565
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-01-06 18:18:40 -0800

    exportds

--- csm_client_core/lib/src/models/models_module.dart
+++ csm_client_core/lib/src/models/models_module.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: directives_ordering
+
 //! --> Exporting [abstractions]
 export 'abstractions/interfaces/iview_filter.dart';
 export 'abstractions/interfaces/iview_filter_node.dart';
@@ -7,3 +9,7 @@ export 'inputs/view_input.dart';
 
 //! --> Exporting [outputs]
 export 'outputs/view_output.dart';
+
+//! --> Exporting 
+export 'view_ordering.dart';
+export 'view_filter_date.dart';
